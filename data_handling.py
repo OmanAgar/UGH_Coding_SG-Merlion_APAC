dcc98735d50e0ec3385dc5dc0e4595a46d039d31
OmanAgar <hanjoxi@gmail.com>
2023-09-17 08:29:00 +0800

    Update data_handling.py

--- data_handling.py
+++ data_handling.py
@@ -1,1 +1,17 @@
+from rider import *
+from route import *
+from driver import *
+import sqlite3
 
+class Database():
+	def __init__(self):
+		self.open()
+	def open(self):
+        """Initializes the database"""
+        self.conn = sqlite3.connect('database.db')
+        self.cur = self.conn.cursor()
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS economy (
+            drivers INTEGER NOT NULL PRIMARY KEY,
+            riders INTEGER NOT NULL DEFAULT 0,
+            credits INTEGER NOT NULL DEFAULT 0
+        )""")
