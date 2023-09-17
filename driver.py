fa2a81469e19fa6976dd27cddb05b6b41414675d
OmanAgar <hanjoxi@gmail.com>
2023-09-17 08:29:31 +0800

    Add Route class, Rider class, db

--- driver.py
+++ driver.py
@@ -1,22 +1,20 @@
 class Driver:
-    def __init__(self):
-        self.info = {"First Name":"N/A","Last Name":"N/A","Age":0,
-                    "Phone Number":"N/A","Vehicle Number":"N/A","People":0,"Notice":"N/A"}
-        self.vehicle = {"Model":"N/A,"Seats":0}
+    def __init__(self,fn,ln,a,pn,vn,p,n,m,s):
+        self.info = {"First Name":fn,"Last Name":ln,"Age":a,
+                    "Phone Number":pn,"Vehicle Number":vn,"People":p,"Notice":n}
+        self.vehicle = {"Model":m,"Seats":s}
     def return_info(self):
         return self.info
     def return_vehicle(self):
         return self.vehicle
-    def add_info(self,fn,ln,a,pn,vn,p,n):
-        self.info = {"First Name":fn,"Last Name":ln,"Age":a,
-                    "Phone Number":pn,"Vehicle Number":vn,"People":p,"Notice":n}
-        return
-    def add_vehicle(self,m,s):
-        self.vehicle = {"Model":m,"Seats":s}
-        return
+    def change_info(self,content,index):
+        self.info.insert(content,index)
+        self.info.pop(index+1)
+        return self.info
+    def change_info(self,content,index):
+        self.info.insert(content,index)
+        self.info.pop(index+1)
+        return self.vehicle
 
-driverA = Driver
-driverA.add_info(driverA,"Joxi","Han",14,"+65 8123 4567","ABC 1234D",3,"Dropping off another passenger")
-driverA.add_vehicle(driverA,"Toyota 86",4)
-print(str(driverA.return_info(driverA))+"\n"+
-      str(driverA.return_vehicle(driverA)))
+def add_driver(Driver,fn,ln,a,pn,vn,p,n,m,s):
+    return Driver(fn,ln,a,pn,vn,p,n,m,s)
--- route.py
+++ route.py
@@ -1,11 +1,31 @@
 import osmapi as osm , bs4 as bs , openrouteservice as ors , math
 ors_key = "5b3ce3597851110001cf62488f97a0c484214cacb893f3be729f251a"
 
-def calc_duration(coords):
-    client = ors.Client(key=ors_key)
-    routes = client.directions(coords)
-    return routes
-coords use long1,lat1,long2,lat2
-coords = ((103.840724,1.427385),(103.835380,1.429710))
-print(f"Distance: {math.ceil(calc_duration(coords)['routes'][0]['summary']['distance']/1000*1000)/1000}km\n"+
-      f"ETA: {math.ceil(calc_duration(coords)['routes'][0]['summary']['duration']/60*100)/100} mins")
+# coords use long1,lat1,long2,lat2
+# coords = ((103.840724,1.427385),(103.835380,1.429710))
+# print(f"Distance: {math.ceil(calc_duration(coords)['routes'][0]['summary']['distance']/1000*1000)/1000}km\n"+
+#       f"ETA: {math.ceil(calc_duration(coords)['routes'][0]['summary']['duration']/60*100)/100} mins")
+
+class Route:
+    def __init__(self,lo1,la1,lo2,la2,driver,rider):
+        self.lo1 = lo1
+        self.la1 = la1
+        self.la2 = la2
+        self.lo2 = lo2
+        self.formatted = ((lo1,la1),(lo2,la2))
+        self.driver = driver
+        self.rider = rider
+    def setup_routes(self):
+        try:
+            client = ors.Client(key=ors_key)
+            self.routes = client.directions(self.formatted)
+            return True
+        except:
+            return False
+    def return_duration(self):
+        return math.ceil(self.routes['routes'][0]['summary']['duration']/60*100)/100
+    def return_distance(self):
+        return math.ceil(self.routes['routes'][0]['summary']['distance']/1000*1000)/1000
+
+def make_route(lo1,la1,lo2,la2,driver,rider):
+    return Route(lo1,la1,lo2,la2,driver,rider)
