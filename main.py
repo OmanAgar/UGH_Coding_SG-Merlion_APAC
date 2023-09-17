b64bd13724ddf9719070ea4efc7ecbbfc1ad1145
OmanAgar <hanjoxi@gmail.com>
2023-09-17 10:10:17 +0800

    ss

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import objects
 
 
+
+#********INITIALISATION********#
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'testing123'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+
+
 #********OBJECTS*********#
 import osmapi as osm , bs4 as bs , openrouteservice as ors , math
 ors_key = "5b3ce3597851110001cf62488f97a0c484214cacb893f3be729f251a"
@@ -23,7 +36,7 @@ class User(UserMixin, db.Model):
 class Driver(db.Model):
     firstname = db.Column(db.String(100), nullable = False)
     lastname = db.Column(db.String(100), nullable = False)
-    age = db.Column(db.Integer, nullable = False)
+    age = db.Column(db.Integer, nullable = False,unique = True,primary_key = True)
     phonenumber = db.Column(db.Integer, nullable = False)
     vehiclenumber = db.Column(db.String(100), nullable = False)
     peopleallowedtocarry = db.Column(db.Integer, nullable = False)
@@ -34,29 +47,17 @@ class Rider(db.Model):
     firstname = db.Column(db.String(100), nullable = False)
     lastname = db.Column(db.String(100), nullable = False)
     age = db.Column(db.Integer, nullable = False)
-    phonenumber = db.Column(db.Integer, nullable = False)
+    phonenumber = db.Column(db.Integer, nullable = False,unique = True,primary_key = True)
 
 # coords use long1,lat1,long2,lat2
 class Route(db.Model):
-    def __init__(self,lo1,la1,lo2,la2,driver,rider):
-        lo1 = db.Column(Float, nullable = False)
-        la1 = db.Column(Float, nullable = False)
-        lo2 = db.Column(Float, nullable = False)
-        la2 = db.Column(Float, nullable = False)
-        self.driver = db.Column(db.String(100), nullable = False)
-        self.rider = db.Column(db.String(100), nullable = False)
+    lo1 = db.Column(db.Float, nullable = False)
+    la1 = db.Column(db.Float, nullable = False)
+    lo2 = db.Column(db.Float, nullable = False)
+    la2 = db.Column(db.Float, nullable = False)
+    driver = db.Column(db.String(100), nullable = False,unique = True,primary_key = True)
+    rider = db.Column(db.String(100), nullable = False)
 
 with app.app_context():
     #db.drop_all()
     db.create_all()
-
-#********INITIALISATION********#
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SECRET_KEY'] = 'testing123'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
-
