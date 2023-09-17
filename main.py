e5359d3b2c6b50b94656c42b256502aad17aa577
OmanAgar <hanjoxi@gmail.com>
2023-09-17 09:59:00 +0800

    ss

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, redirect, render_template, request, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from objects
+os.system("objects.py")
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SECRET_KEY'] = 'testing123'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
